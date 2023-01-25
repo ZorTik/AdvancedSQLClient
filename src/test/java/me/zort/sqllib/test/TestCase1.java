bab6f767fb980b868a179b8e5a33f1e5847be321
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-01-25 17:43:15 +0100

    Test

--- src/test/java/me/zort/sqllib/test/TestCase1.java
+++ src/test/java/me/zort/sqllib/test/TestCase1.java
@@ -3,6 +3,8 @@ package me.zort.sqllib.test;
 import me.zort.sqllib.SQLConnectionBuilder;
 import me.zort.sqllib.SQLDatabaseOptions;
 import me.zort.sqllib.api.SQLDatabaseConnection;
+import me.zort.sqllib.api.data.QueryRowsResult;
+import me.zort.sqllib.api.provider.Select;
 import me.zort.sqllib.internal.impl.DefaultSQLEndpoint;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestCase1 {
 
     private SQLDatabaseConnection connection;
+    private final User user1 = new User("User1", 100);
+    private final User user2 = new User("User2", 200);
 
     @BeforeAll
     public void prepare() {
@@ -26,8 +30,6 @@ public class TestCase1 {
 
         if(host == null)
             host = "localhost";
-
-        System.out.println("Using host: " + host);
 
         SQLDatabaseOptions options = new SQLDatabaseOptions();
         options.setDebug(true);
@@ -41,6 +43,7 @@ public class TestCase1 {
         assertEquals(endpoint.buildJdbc(), String.format("jdbc:mysql://%s:3306/test", host));
         assertTrue(connection.connect());
         assertTrue(connection.isConnected());
+        assertTrue(connection.query(() -> "CREATE TABLE IF NOT EXISTS users (nickname VARCHAR(16) PRIMARY KEY NOT NULL, points INT NOT NULL);").isSuccessful());
     }
 
     @AfterAll
@@ -50,12 +53,61 @@ public class TestCase1 {
 
     @Test
     public void testUpsert() {
-
+        assertTrue(connection.save("users", user1).isSuccessful());
+        assertTrue(connection.upsert()
+                .into("users", "nickname", "points")
+                .values(user2.getNickname(), user2.getPoints())
+                .onDuplicateKey()
+                .and("nickname", user2.getNickname())
+                .and("points", user2.getPoints())
+                .execute().isSuccessful());
     }
 
     @Test
     public void testSelect() {
+        QueryRowsResult<User> result = connection.query(Select.of().from("users")
+                .where()
+                .isEqual("nickname", "User1"), User.class);
 
+        assertTrue(result.isSuccessful());
+        assertEquals(1, result.size());
+        assertEquals(user1, result.get(0));
+    }
+
+    private static class User {
+        private final String nickname;
+        private final int points;
+
+        public User(String nickname, int points) {
+            this.nickname = nickname;
+            this.points = points;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public int getPoints() {
+            return points;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User user = (User) o;
+
+            if (points != user.points) return false;
+            return nickname.equals(user.nickname);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = nickname.hashCode();
+            result = 31 * result + points;
+            return result;
+        }
     }
 
 }
