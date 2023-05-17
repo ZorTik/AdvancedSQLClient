e10c4fce580f28ecd42a017a9d79bbbcc0c5e9d9
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-05-17 23:07:32 +0200

    Code reformat

--- core/src/main/java/me/zort/sqllib/mapping/MappingRegistryImpl.java
+++ core/src/main/java/me/zort/sqllib/mapping/MappingRegistryImpl.java
@@ -1,31 +1,20 @@
 package me.zort.sqllib.mapping;
 
-import lombok.Getter;
-import lombok.Setter;
-import me.zort.sqllib.SQLDatabaseConnection;
+import lombok.RequiredArgsConstructor;
 import me.zort.sqllib.SQLDatabaseConnectionImpl;
-import me.zort.sqllib.SQLiteDatabaseConnectionImpl;
 import me.zort.sqllib.api.mapping.MappingProxyInstance;
 import me.zort.sqllib.api.mapping.StatementMappingRegistry;
-import me.zort.sqllib.api.model.SchemaSynchronizer;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@RequiredArgsConstructor
 public class MappingRegistryImpl implements StatementMappingRegistry {
 
     private final Map<Class<?>, MappingProxyInstance<?>> proxyWrappers = new ConcurrentHashMap<>();
     private final SQLDatabaseConnectionImpl connection;
-    @Setter
-    @Getter
-    private SchemaSynchronizer<SQLDatabaseConnection> synchronizer;
-
-    public MappingRegistryImpl(SQLDatabaseConnectionImpl connection, SchemaSynchronizer<SQLDatabaseConnection> synchronizer) {
-        this.connection = connection;
-        this.synchronizer = synchronizer;
-    }
 
     @Override
     public void registerProxy(MappingProxyInstance<?> proxyInstance) {
