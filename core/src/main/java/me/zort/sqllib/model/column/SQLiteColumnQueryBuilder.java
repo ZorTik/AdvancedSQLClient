c80dee14ca01d66172d6010d97a3934866c6f6d2
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-05-20 18:40:07 +0200

    SQLiteColumnQueryBuilder

--- core/src/main/java/me/zort/sqllib/model/column/SQLiteColumnQueryBuilder.java
+++ core/src/main/java/me/zort/sqllib/model/column/SQLiteColumnQueryBuilder.java
@@ -12,8 +12,16 @@ public class SQLiteColumnQueryBuilder extends InnoColumnQueryBuilder {
     public String buildActionQuery(ColumnAction action, String table, ColumnDefinition from, ColumnDefinition to) {
         if (action.equals(ColumnAction.MODIFY)) {
             TableSchema schema = connection.getSchemaBuilder(table).buildTableSchema();
+            String[] newDefinitions = new String[schema.size()];
+            for (int i = 0; i < schema.size(); i++) {
+                if (schema.getDefinitionName(i).equals(from.getName())) {
+                    newDefinitions[i] = from.getName() + " " + from.getType();
+                } else {
+                    newDefinitions[i] = schema.getDefinition(i);
+                }
+            }
             String queries = "ALTER TABLE " + table + " RENAME TO " + table + "_old;";
-            queries += "CREATE TABLE " + table + "(" + String.join(", ", schema.getDefinitions()) + ");";
+            queries += "CREATE TABLE " + table + "(" + String.join(", ", newDefinitions) + ");";
             queries += "INSERT INTO " + table + "(" + String.join(", ", schema.getDefinitionNames()) + ") SELECT " + String.join(", ", schema.getDefinitionNames()) + " FROM " + table + "_old;";
             queries += "DROP TABLE " + table + "_old;";
             return queries;
