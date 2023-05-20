7d698b02799157b1c5bc35a04cb887e93c13aa56
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-05-20 18:17:17 +0200

    SQLiteColumnQueryBuilder

--- core/src/main/java/me/zort/sqllib/model/column/SQLiteColumnQueryBuilder.java
+++ core/src/main/java/me/zort/sqllib/model/column/SQLiteColumnQueryBuilder.java
@@ -12,7 +12,7 @@ public class SQLiteColumnQueryBuilder extends InnoColumnQueryBuilder {
     public String buildActionQuery(ColumnAction action, String table, ColumnDefinition from, ColumnDefinition to) {
         if (action.equals(ColumnAction.MODIFY)) {
             TableSchema schema = connection.getSchemaBuilder(table).buildTableSchema();
-            String queries = "ALTER TABLE" + table + " RENAME TO " + table + "_old;";
+            String queries = "ALTER TABLE " + table + " RENAME TO " + table + "_old;";
             queries += "CREATE TABLE " + table + "(" + String.join(", ", schema.getDefinitions()) + ");";
             queries += "INSERT INTO " + table + "(" + String.join(", ", schema.getDefinitionNames()) + ") SELECT " + String.join(", ", schema.getDefinitionNames()) + " FROM " + table + "_old;";
             queries += "DROP TABLE " + table + "_old;";
